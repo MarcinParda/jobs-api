cb36e6492c46e65cde3a920da86905e390b47873
MarcinParda <marcin98parda@gmail.com>
2022-05-14 20:07:22 +0200

    passed token to response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,12 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const { BadReqeustError, BadRequestError } = require('../errors');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   const user = await User.create({...req.body});
-  res.status(StatusCodes.CREATED).json({user});
+  const token = jwt.sign({ userId: user._id, name: user.name }, process.env.JWT_SECRET, { expiresIn: '30d' });
+  res.status(StatusCodes.CREATED).json({token, user: {name: user.name}});
 };
 
 const login = async (req, res) => {
