d0a665dae6ed442960d766c5a970a4b2e20d9b35
MarcinParda <marcin98parda@gmail.com>
2022-05-14 13:39:56 +0200

    added middleware handling cripting password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
   name: {
@@ -24,5 +25,11 @@ const UserSchema = new Schema({
     select: false,
   },
 }) 
+
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+  
 
 module.exports = mongoose.model('User', UserSchema);
