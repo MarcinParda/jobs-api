42b79aa2decc6c1c30670dbdaf1c2cfb5e1467de
MarcinParda <marcin98parda@gmail.com>
2022-05-14 23:09:12 +0200

    delete job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -49,7 +49,21 @@ const updateJob = async (req, res) => {
 };
 
 const deleteJob = async (req, res) => {
-  res.send('delete job');
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOneAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
