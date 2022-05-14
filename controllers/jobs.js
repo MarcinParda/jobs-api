9421026a8f17b970c304b91a8b94703aa7c46f6b
MarcinParda <marcin98parda@gmail.com>
2022-05-14 22:33:28 +0200

    created logic for get job endpoint

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,6 @@
 const Job = require('../models/Job');
 const { StatusCodes } = require('http-status-codes');
+const { NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
@@ -7,10 +8,15 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send('get job');
+  const { user: { userId }, params: {id: jobId} } = req;
+  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!job) {
+    throw new NotFoundError('Job not found');
+  }
+  res.status(StatusCodes.OK).json({job});
 };
-
-const createJob = async (req, res) => {
+ 
+const createJob = async (req, res) => { 
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({job});
