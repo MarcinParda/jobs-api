b522ed3b63e8a2fde6b915716a4d2b6a3c7812fc
MarcinParda <marcin98parda@gmail.com>
2022-05-14 20:23:55 +0200

    created env variables for handling JWT

--- models/User.js
+++ models/User.js
@@ -33,7 +33,7 @@ UserSchema.pre('save', async function () {
 });
 
 UserSchema.methods.createJWT = function () {
-  return jwt.sign({userId: this._id, name: this.name}, 'jwtSecret', { expiresIn: '30d' });
+  return jwt.sign({userId: this._id, name: this.name}, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 }
   
 
