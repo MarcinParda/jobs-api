8556a27a728081aebff676a310cc1d01e5078a4d
MarcinParda <marcin98parda@gmail.com>
2022-05-14 23:27:39 +0200

    added custom error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,21 @@
-const { CustomAPIError } = require("../errors");
-const { StatusCodes } = require("http-status-codes");
+const { CustomAPIError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Internal server error',
+  };
+
+  console.log(err.code, err.keyValue);
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field please enter a unique value`;
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
