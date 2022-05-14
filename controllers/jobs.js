61a8f3cf177d0d8bc942161690eda5414b828b8d
MarcinParda <marcin98parda@gmail.com>
2022-05-14 23:04:59 +0200

    update job functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,9 @@
-const Job = require("../models/Job");
-const { StatusCodes } = require("http-status-codes");
-const { NotFoundError } = require("../errors");
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { NotFoundError, BadRequestError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt');
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
@@ -12,9 +12,12 @@ const getJob = async (req, res) => {
     user: { userId },
     params: { id: jobId },
   } = req;
-  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
   if (!job) {
-    throw new NotFoundError("Job not found");
+    throw new NotFoundError(`No job with id ${jobId}`);
   }
   res.status(StatusCodes.OK).json({ job });
 };
@@ -27,18 +30,26 @@ const createJob = async (req, res) => {
 
 const updateJob = async (req, res) => {
   const {
+    body: { company, position },
     user: { userId },
     params: { id: jobId },
   } = req;
-  const job = await Job.findOne({ _id: jobId, createdBy: userId });
+  if (!company || !position) {
+    throw new BadRequestError('Company or position cannot be empty');
+  }
+  const job = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
   if (!job) {
-    throw new NotFoundError("Job not found");
+    throw new NotFoundError(`No job with id ${jobId}`);
   }
   res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete job");
+  res.send('delete job');
 };
 
 module.exports = {
