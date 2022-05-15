c0a7c02c2a19f4c248204955c0f3ca607af25223
MarcinParda <marcin98parda@gmail.com>
2022-05-15 18:55:05 +0200

    handling cast error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,8 +6,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Internal server error',
   };
 
-  console.log(err.code, err.keyValue);
-
   if (err.name === 'ValidationError') {
     customError.msg = Object.values(err.errors)
       .map((error) => error.message)
@@ -20,6 +18,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
       err.keyValue
     )} field please enter a unique value`;
     customError.statusCode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `${err.value} is not a valid ${err.kind}`;
+    customError.statusCode = 404;
   }
 
   return res.status(customError.statusCode).json({ msg: customError.msg });
