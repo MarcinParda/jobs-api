35bd35f324992c74315670233d377d88062bf0b0
MarcinParda <marcin98parda@gmail.com>
2022-05-15 19:28:36 +0200

    added security middlewares

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-require("dotenv").config();
-require("express-async-errors");
-const express = require("express");
+require('dotenv').config();
+require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+const express = require('express');
 const app = express();
 
 // connect DB
-const connectDB = require("./db/connect");
-const authenticateUser = require("./middleware/authentication");
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
 
 // routers
-const authRouter = require("./routes/auth");
-const jobsRouter = require("./routes/jobs");
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 
 // error handler
-const notFoundMiddleware = require("./middleware/not-found");
-const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
